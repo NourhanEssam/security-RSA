825760489d0165380976e58f94cccd218015a6c3
NourhanEssam <nourhanessam94@gmail.com>
2017-02-01 13:45:35 +0200

    Try init

--- security-RSA.cpp
+++ security-RSA.cpp
@@ -8,9 +8,31 @@ using namespace std;
 class BigInteger
 {
 private:
-	string NumberString;
-	std::vector<long long int> NumberInteger;
+    string NumberString;
+    std::vector<long long int> NumberInteger;
 public:
     BigInteger() {}
+    BigInteger(string S)
+    {
+        NumberString = S;
+        while (NumberString.length() > 0) {
+            if(NumberString.length()<19)
+            {cout<< NumberString; break;}
+            else
+            {
+                string ss = NumberString.substr(NumberString.length()-18,NumberString.length());
+                long long int hh = stoll(ss);
+                cout << NumberString.substr(NumberString.length()-18,NumberString.length())<<endl;
+                NumberString.erase(NumberString.length()-18,NumberString.length());
+            }
+        }
+    }
 };
 
+int main()
+{
+    string ii;
+    cin>>ii;
+    BigInteger i = BigInteger(ii);
+    return 0;
+}
