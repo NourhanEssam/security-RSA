408351269106881d4ee34911938e218bf5323c2e
NourhanEssam <nourhanessam94@gmail.com>
2017-02-01 12:03:19 +0200

    Class initialized

--- security-RSA.cpp
+++ security-RSA.cpp
@@ -1,2 +1,16 @@
 //g++ inputfile.cpp -o outputbinary
-//#include <cstdlib>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class BigInteger
+{
+private:
+	string NumberString;
+	std::vector<long long int> NumberInteger;
+public:
+    BigInteger() {}
+};
+
