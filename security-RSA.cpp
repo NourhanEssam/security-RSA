c75f49bc521ac3613fc02d05899dfa43074da976
NourhanEssam <nourhanessam94@gmail.com>
2017-02-10 14:33:02 +0200

    arrays used .. constructor and showContent

--- security-RSA.cpp
+++ security-RSA.cpp
@@ -10,151 +10,155 @@
 using namespace std;
 
 #define PRECISION 18
-#define MAX 1000000000000000000
+#define MAX_Mul 1000000000000000000
+#define MAX 1000000000
+#define capacity 40
 
 class BigInteger
 {
 private:
     string NumberString;
-    vector<long long int> NumberInteger;
+    long long int NumberInteger[capacity] = {0};
+    //vector<long long int> NumberInteger;
+    bool isNegative;
 public:
     BigInteger() {}
     BigInteger(string S)
     {
-        NumberString = S;
+        int i = 0;
+        NumberString = S;        
         while (NumberString.length() > 0) {
-            if(NumberString.length()<=PRECISION-1)
+            if(NumberString.length()<=(PRECISION/2)-1)
             {
-                NumberInteger.push_back(stoll(NumberString));
+                NumberInteger[capacity-1-i++] = stoll(NumberString);
                 break;
             }
             else
             {
-                NumberInteger.push_back(stoll(NumberString.substr(NumberString.length()-PRECISION,NumberString.length())));
-                NumberString.erase(NumberString.length()-PRECISION,NumberString.length());
+                NumberInteger[capacity-1-i++] = stoll(NumberString.substr(NumberString.length()-PRECISION/2,NumberString.length()));
+                NumberString.erase(NumberString.length()-PRECISION/2,NumberString.length());
             }
         }
     }
     void ShowContent()
     {
-            vector<long long int>::reverse_iterator iterator;
-            for(iterator = NumberInteger.rbegin(); iterator<NumberInteger.rend(); iterator++)
+        for(int i=0;i<capacity;i++)
             {
                 cout.fill( '0' );    
-                cout.width( 18 );
-                cout<<*iterator;
+                cout.width( PRECISION/2 );
+                cout<<NumberInteger[i];
             }
             cout<<endl;
     }
-    BigInteger operator+(const BigInteger& SecondNumber)
-    {
-        BigInteger result;
-        vector<long long int>::const_iterator iterator = NumberInteger.begin();
-        vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
-        int carry = 0;
-        while(1)
-        {
-			if(iterator==NumberInteger.end())
-			{
-                if(Seconditerator==SecondNumber.NumberInteger.end()) break;
-				result.NumberInteger.push_back(*Seconditerator);
-                Seconditerator++;
-			}
-			else if(Seconditerator==SecondNumber.NumberInteger.end())
-			{
-                if(iterator==NumberInteger.end()) break;
-				result.NumberInteger.push_back(*iterator);
-				iterator++;
-			}
-            else if((*iterator + *Seconditerator+carry)<MAX)
-            {
-                result.NumberInteger.push_back(*iterator + *Seconditerator+carry);
-                carry = 0;
-				iterator++;
-                Seconditerator++;
-            }
-            else if((*iterator + *Seconditerator+carry)>=MAX)
-            {
-                result.NumberInteger.push_back((*iterator + *Seconditerator+carry)%MAX);
-                carry = 1;
-				iterator++;
-                Seconditerator++;
-            }
-        }
-        cout<<"Supposed to add" <<endl;
-        return result;
-    }
 
-    BigInteger operator-(const BigInteger& SecondNumber)
-    {
-        BigInteger result;
-        vector<long long int>::const_iterator iterator = NumberInteger.begin();
-        vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
-        int carry = 0;
-        while(1)
-        {
-            if(Seconditerator==SecondNumber.NumberInteger.end())
-            {
-                if(iterator==NumberInteger.end()) break;
-                result.NumberInteger.push_back(*iterator);
-                iterator++;
-            }
-            else if((*iterator - *Seconditerator+carry)>=0)
-            {
-                result.NumberInteger.push_back(*iterator - *Seconditerator+carry);
-                carry = 0;
-                iterator++;
-                Seconditerator++;
-            }
-            else if((*iterator - *Seconditerator+carry)<0)
-            {
-                result.NumberInteger.push_back((*iterator - *Seconditerator+carry+MAX));
-                carry = -1;
-                iterator++;
-                Seconditerator++;
-            }
-        }
-        cout<<"Supposed to subtract" <<endl;
-        return result;
-    }
+   //  BigInteger operator+(const BigInteger& SecondNumber)
+   //  {
+   //      BigInteger result;
+   //      int carry = 0;
+   //      int i=0,j=0;
+   //      while(1)
+   //      {
+			// if(iterator==NumberInteger.end())
+			// {
+   //              if(Seconditerator==SecondNumber.NumberInteger.end()) break;
+			// 	result.NumberInteger.push_back(*Seconditerator);
+   //              Seconditerator++;
+			// }
+			// else if(Seconditerator==SecondNumber.NumberInteger.end())
+			// {
+   //              if(iterator==NumberInteger.end()) break;
+			// 	result.NumberInteger.push_back(*iterator);
+			// 	iterator++;
+			// }
+   //          else if((*iterator + *Seconditerator+carry)<MAX)
+   //          {
+   //              result.NumberInteger.push_back(*iterator + *Seconditerator+carry);
+   //              carry = 0;
+			// 	iterator++;
+   //              Seconditerator++;
+   //          }
+   //          else if((*iterator + *Seconditerator+carry)>=MAX)
+   //          {
+   //              result.NumberInteger.push_back((*iterator + *Seconditerator+carry)%MAX);
+   //              carry = 1;
+			// 	iterator++;
+   //              Seconditerator++;
+   //          }
+   //      }
+   //      cout<<"Supposed to add" <<endl;
+   //      return result;
+   //  }
 
-    BigInteger operator*(const BigInteger& SecondNumber)
-    {
-        BigInteger result;
-        vector<long long int>::const_iterator iterator = NumberInteger.begin();
-        vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
-        int carry;
-        for (vector<long long int>::const_iterator iterator=NumberInteger.begin(); iterator < NumberInteger.end(); iterator++)
-        {
-            carry = 0;
-            BigInteger temp;
-            cout<<"temp fady  ";
-                 temp.ShowContent();
-            for (vector<long long int>::const_iterator Seconditerator=SecondNumber.NumberInteger.begin(); Seconditerator < SecondNumber.NumberInteger.end(); Seconditerator++)
-            {
-                if(((*iterator * *Seconditerator)+carry) >=MAX)
-                {
-                    carry = 1;
-                }
-            else
-            {
-                carry = 0;
-            }
-            temp.NumberInteger.push_back(((*iterator * *Seconditerator)+carry)%MAX);
-                 //temp.NumberInteger.push_back(*iterator * *Seconditerator);
-                // result = result + (*iterator * *Seconditerator);
-                 //result = result + temp + carry;
-                 cout<<"temp  ";
-                 temp.ShowContent();
-                //temp.NumberInteger.erase(temp.NumberInteger.begin());
-            }
-            result = result + temp;
-            cout<<"result  ";
-                result.ShowContent();
-        }
-        cout<<"Supposed to Multiply" <<endl;
-        return result;
-    }
+   //  BigInteger operator-(const BigInteger& SecondNumber)
+   //  {
+   //      BigInteger result;
+   //      vector<long long int>::const_iterator iterator = NumberInteger.begin();
+   //      vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
+   //      int carry = 0;
+   //      while(1)
+   //      {
+   //          if(Seconditerator==SecondNumber.NumberInteger.end())
+   //          {
+   //              if(iterator==NumberInteger.end()) break;
+   //              result.NumberInteger.push_back(*iterator);
+   //              iterator++;
+   //          }
+   //          else if((*iterator - *Seconditerator+carry)>=0)
+   //          {
+   //              result.NumberInteger.push_back(*iterator - *Seconditerator+carry);
+   //              carry = 0;
+   //              iterator++;
+   //              Seconditerator++;
+   //          }
+   //          else if((*iterator - *Seconditerator+carry)<0)
+   //          {
+   //              result.NumberInteger.push_back((*iterator - *Seconditerator+carry+MAX));
+   //              carry = -1;
+   //              iterator++;
+   //              Seconditerator++;
+   //          }
+   //      }
+   //      cout<<"Supposed to subtract" <<endl;
+   //      return result;
+   //  }
+
+   //  BigInteger operator*(const BigInteger& SecondNumber)
+   //  {
+   //      BigInteger result;
+   //      vector<long long int>::const_iterator iterator = NumberInteger.begin();
+   //      vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
+   //      int carry;
+   //      for (vector<long long int>::const_iterator iterator=NumberInteger.begin(); iterator < NumberInteger.end(); iterator++)
+   //      {
+   //          carry = 0;
+   //          BigInteger temp;
+   //          cout<<"temp fady  ";
+   //               temp.ShowContent();
+   //          for (vector<long long int>::const_iterator Seconditerator=SecondNumber.NumberInteger.begin(); Seconditerator < SecondNumber.NumberInteger.end(); Seconditerator++)
+   //          {
+   //              if(((*iterator * *Seconditerator)+carry) >=MAX)
+   //              {
+   //                  carry = 1;
+   //              }
+   //          else
+   //          {
+   //              carry = 0;
+   //          }
+   //          temp.NumberInteger.push_back(((*iterator * *Seconditerator)+carry)%MAX);
+   //               //temp.NumberInteger.push_back(*iterator * *Seconditerator);
+   //              // result = result + (*iterator * *Seconditerator);
+   //               //result = result + temp + carry;
+   //               cout<<"temp  ";
+   //               temp.ShowContent();
+   //              //temp.NumberInteger.erase(temp.NumberInteger.begin());
+   //          }
+   //          result = result + temp;
+   //          cout<<"result  ";
+   //              result.ShowContent();
+   //      }
+   //      cout<<"Supposed to Multiply" <<endl;
+   //      return result;
+   //  }
 };
 
 int main()
@@ -162,9 +166,10 @@ int main()
     string ii;
     cin>>ii;
     BigInteger i = BigInteger(ii);
-    cin>>ii;
-    BigInteger i2 = BigInteger(ii);
-    BigInteger i3 = i*i2;
-    i3.ShowContent();
+    i.ShowContent();
+    // cin>>ii;
+    // BigInteger i2 = BigInteger(ii);
+    // BigInteger i3 = i*i2;
+    // i3.ShowContent();
     return 0;
 }
