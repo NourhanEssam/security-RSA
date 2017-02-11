b8cd53dc536066cf17c4618087c8b450f774a97d
NourhanEssam <nourhanessam94@gmail.com>
2017-02-11 23:48:32 +0200

    sub without neg

--- security-RSA.cpp
+++ security-RSA.cpp
@@ -19,7 +19,7 @@ class BigInteger
 private:
     string NumberString;
     long long int NumberInteger[capacity] = {0};
-    bool isNegative;
+    bool isNegative = false;
     int size;
 public:
     BigInteger() {size=0;}
@@ -82,87 +82,65 @@ public:
         return result;
     }
 
-    //  BigInteger operator-(const BigInteger& SecondNumber)
-    //  {
-    //      BigInteger result;
-    //      vector<long long int>::const_iterator iterator = NumberInteger.begin();
-    //      vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
-    //      int carry = 0;
-    //      while(1)
-    //      {
-    //          if(Seconditerator==SecondNumber.NumberInteger.end())
-    //          {
-    //              if(iterator==NumberInteger.end()) break;
-    //              result.NumberInteger.push_back(*iterator);
-    //              iterator++;
-    //          }
-    //          else if((*iterator - *Seconditerator+carry)>=0)
-    //          {
-    //              result.NumberInteger.push_back(*iterator - *Seconditerator+carry);
-    //              carry = 0;
-    //              iterator++;
-    //              Seconditerator++;
-    //          }
-    //          else if((*iterator - *Seconditerator+carry)<0)
-    //          {
-    //              result.NumberInteger.push_back((*iterator - *Seconditerator+carry+MAX));
-    //              carry = -1;
-    //              iterator++;
-    //              Seconditerator++;
-    //          }
-    //      }
-    //      cout<<"Supposed to subtract" <<endl;
-    //      return result;
-    //  }
-
-    //  BigInteger operator*(const BigInteger& SecondNumber)
-    //  {
-    //      BigInteger result;
-    //      vector<long long int>::const_iterator iterator = NumberInteger.begin();
-    //      vector<long long int>::const_iterator Seconditerator = SecondNumber.NumberInteger.begin();
-    //      int carry;
-    //      for (vector<long long int>::const_iterator iterator=NumberInteger.begin(); iterator < NumberInteger.end(); iterator++)
-    //      {
-    //          carry = 0;
-    //          BigInteger temp;
-    //          cout<<"temp fady  ";
-    //               temp.ShowContent();
-    //          for (vector<long long int>::const_iterator Seconditerator=SecondNumber.NumberInteger.begin(); Seconditerator < SecondNumber.NumberInteger.end(); Seconditerator++)
-    //          {
-    //              if(((*iterator * *Seconditerator)+carry) >=MAX)
-    //              {
-    //                  carry = 1;
-    //              }
-    //          else
-    //          {
-    //              carry = 0;
-    //          }
-    //          temp.NumberInteger.push_back(((*iterator * *Seconditerator)+carry)%MAX);
-    //               //temp.NumberInteger.push_back(*iterator * *Seconditerator);
-    //              // result = result + (*iterator * *Seconditerator);
-    //               //result = result + temp + carry;
-    //               cout<<"temp  ";
-    //               temp.ShowContent();
-    //              //temp.NumberInteger.erase(temp.NumberInteger.begin());
-    //          }
-    //          result = result + temp;
-    //          cout<<"result  ";
-    //              result.ShowContent();
-    //      }
-    //      cout<<"Supposed to Multiply" <<endl;
-    //      return result;
-    //  }
+     BigInteger operator-(const BigInteger& SecondNumber)
+     {
+         BigInteger result;
+         int carry=0,i=0,j=0;
+         while(1)
+         {
+            if(size == SecondNumber.size)
+            {
+                if(NumberInteger[capacity-1]<SecondNumber.NumberInteger[capacity-1])
+                {
+                    result.isNegative = true;
+                    //swap
+                    cout <<"swap";
+                }
+            }
+            else if(size<SecondNumber.size)
+            {
+                result.isNegative = true;
+                //swap
+                cout<<"swap";
+            }
+        
+          // if(i==size)
+          //   {
+          //       if(j==SecondNumber.size) break;
+          //       result.NumberInteger[capacity-1-result.size++] = SecondNumber.NumberInteger[capacity-1-j++];
+          //   }
+            if(j==SecondNumber.size)
+            {
+                if(i==size) break;
+                result.NumberInteger[capacity-1-result.size++] = NumberInteger[capacity-1-i++];
+            }
+            else if((NumberInteger[capacity-1-i] - SecondNumber.NumberInteger[capacity-1-j]+carry)>=0)
+            {
+                result.NumberInteger[capacity-1-result.size++] = NumberInteger[capacity-1-i++] - SecondNumber.NumberInteger[capacity-1-j++]+carry;
+                carry = 0;
+            }
+            else if((NumberInteger[capacity-1-i] - SecondNumber.NumberInteger[capacity-1-j]+carry)<0)
+            {
+                result.NumberInteger[capacity-1-result.size++] = NumberInteger[capacity-1-i++] - SecondNumber.NumberInteger[capacity-1-j++]+carry+MAX;
+                carry = -1;
+            }
+}
+         cout<<"Supposed to subtract" <<endl;
+         return result;
+ }
 };
 
 int main()
 {
     string ii = "12369571528747655798110188786567180759626910465726920556567298659370399748072366507234899432827475865189642714067836207300153035059472237275816384410077871";
+    //string ii = "12111";
     //cin>>ii;
     BigInteger i = BigInteger(ii);
     ii = "2065420353441994803054315079370635087865508423962173447811880044936318158815802774220405304957787464676771309034463560633713497474362222775683960029689473";
+    //ii = "1201";
     //cin>>ii;
     BigInteger i2 = BigInteger(ii);
-    BigInteger i3 = i+i2;
+    BigInteger i3 = i-i2;
     i3.ShowContent();
     return 0;
 }
